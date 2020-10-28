742aff53e90982b32013a1be5b04fa8a789857cd
rlaehddus <dongyune0409@naver.com>
2020-10-28 23:11:27 +0900

    first

---
+++ js/dom.js
@@ -0,0 +1,8 @@
+function hideH3P() {
+    document.querySelector("#detail h3").style.visibility="hidden";
+    document.querySelector("#detail p").style.visibility="hidden";
+ }
+ function hideH() {
+    document.querySelector("#detail h3").style.visibility="";
+    document.querySelector("#detail p").style.visibility="";
+ }
